47d6e8545b5ce92d0f96b610fa85349f8c70cfef
john.patton <john.patton@digital.hmrc.gov.uk>
2018-01-22 11:40:19 +0000

    Added function with File IO

--- src/main/scala/App/Main.scala
+++ src/main/scala/App/Main.scala
@@ -1,10 +1,14 @@
 package App
 
+import scala.io.StdIn
+
 object Main extends App {
 
-  def printName(name: String) = {
-    println("Hello " + name)
-  }
+  def printName(name: String, age: String, profession: String) : Unit = println(s"Hello $name! You are $age years old! You are a $profession")
 
-  printName("John")
+  val name = StdIn.readLine("What is your name? ")
+  val age = StdIn.readLine("How old are you? ")
+  val profession = StdIn.readLine("What is your job title? ")
+
+  printName(name, age, profession)
 }
