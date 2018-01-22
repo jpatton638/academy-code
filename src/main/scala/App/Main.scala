f1d90d0460799d107df6ade4adaf91d00bf1e5d0
john.patton <john.patton@digital.hmrc.gov.uk>
2018-01-22 12:11:29 +0000

    Altered conditional expression for function

--- src/main/scala/App/Main.scala
+++ src/main/scala/App/Main.scala
@@ -4,7 +4,15 @@ import scala.io.StdIn
 
 object Main extends App {
 
-  def printName(name: String, age: String, profession: String) : Unit = println(s"Hello $name! You are $age years old! You are a $profession")
+  def printName(name: String, age: String, profession: String) : Unit = {
+
+    if (name == "John") {
+      println("Go away John, I already know you")
+    } else {
+      println(s"Hello $name! You are $age years old! You are a $profession")
+    }
+
+  }
 
   val name = StdIn.readLine("What is your name? ")
   val age = StdIn.readLine("How old are you? ")
