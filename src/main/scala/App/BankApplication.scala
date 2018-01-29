fa3ef0e020b948e99156d2144b7c17584627f3ea
john.patton <john.patton@digital.hmrc.gov.uk>
2018-01-29 10:53:37 +0000

    Added secondary constructors

--- src/main/scala/App/BankApplication.scala
+++ src/main/scala/App/BankApplication.scala
@@ -7,16 +7,13 @@ import scala.io.StdIn
 abstract class BankAccount(accountNumber : String, val balance: Double) {
 
   def withdraw(amount: Double) : BankAccount
-
   def deposit(amount: Double) : BankAccount
-
 }
 
 class SavingsAccount(accountNumber: String,
                      balance : Double) extends BankAccount(accountNumber,balance) {
 
   override def deposit(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance + amount)
-
   override def withdraw(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance - amount)
 
 }
@@ -24,22 +21,24 @@ class SavingsAccount(accountNumber: String,
 class Person(name : String, age: Int, private val bankAccount: BankAccount) {
 
   def this(name: String, age: Int) = this(name, age, new SavingsAccount("12345678", 0.00))
+  def this(name: String) = this(name, 0, new SavingsAccount("87654321", 0.00))
+  def this(firstName: String, lastName: String) = this(s"$firstName $lastName", 0, new SavingsAccount("99998888", 0.00))
+
   private val years : String = if(age == 1) "year" else "years"
 
   def speak() : String = {
     if (name == "John") {
       "You don't get a hello."
     } else {
-      s"Hello $name, you are $age $years old. \n You have ${bankAccount.balance.} in your account"
+      s"Hello $name, you are $age $years old. \nYou have ${bankAccount.balance} in your account"
     }
   }
 
 }
 
-object Prompt {`
+object Prompt {
 
   def ask(message : String) : String = StdIn.readLine(message)
-
 }
 
 object GreetingsApplication extends App {
@@ -49,12 +48,12 @@ object GreetingsApplication extends App {
   val savingsPlus100 = savingsAccount.deposit(100.00)
 
 
-  val name : String = Prompt.ask("What is your name? ")
-  val age : String = Prompt.ask("How old are you? ")
+  val name : String = Prompt.ask("What is your name?\n")
+  val age : String = Prompt.ask("How old are you?\n")
 
   val p : Person = new Person(name, age.toInt)
+  val child = new Person("David")
+  val p2 = new Person("John", "Patton")
 
   println(p.speak())
-
-
 }
