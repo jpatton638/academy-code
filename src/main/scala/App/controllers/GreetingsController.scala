bbd9788c6fe182de4f46dd9f9a0d11bae81863a8
jpatton638 <jpatton638@gmail.com>
2018-02-15 12:47:25 +0000

    Implemented exception handling

--- src/main/scala/App/controllers/GreetingsController.scala
+++ src/main/scala/App/controllers/GreetingsController.scala
@@ -3,35 +3,39 @@ package App.controllers
 import App.models.{CashISASavingsAccount, Person, SavingsAccount}
 import App.views.Prompt
 
-/**
-  * Instantiate everything
-  */
 object GreetingsController extends App {
 
   val savingsAccount = new SavingsAccount("12345", 100.00)
   val savingsPlus100 = savingsAccount.deposit(100.00)
 
 
-  val name : String = Prompt.ask("What is your name?\n")
-  val age : String = Prompt.ask("How old are you?\n")
+  def makePerson: Person = {
+    val name: String = Prompt.ask("What is your name?\n")
+    val ageInput: String = Prompt.ask("How old are you?\n")
+    try {
+      new Person(name, ageInput.toInt)
+    } catch {
+      case _: NumberFormatException => makePerson
+    }
+  }
+
+  val p = makePerson
 
 
-  val p : Person = new Person(name, age.toInt)
-  val child = new Person("David")
-  val p2 = new Person("John", "Patton")
+  println(p.speak())
 
-
-    println(p.speak())
-
-  val cashISA = new CashISASavingsAccount("22334455", 0.00)
-  val isaDeposited = cashISA.deposit(10000.00)
-  val withdrawFromISA = isaDeposited.withdraw(200.00)
-  val personWithCashISA = new Person("Loyal Customer", 56, List(withdrawFromISA))
-
-    println(personWithCashISA.speak())
-
-  val superAccount = new CashISASavingsAccount(accountNumber = "55667788", balance =  0.00, depositThreshold = 1000.00).deposit(1000.00).withdraw(200.00)
-  val superUser = new Person("Bryan", 58, List(superAccount))
-
-  println(superUser.speak())
+//  val p2 = new Person("John", "Patton")
+//  val child = new Person("David")
+//
+//  val cashISA = new CashISASavingsAccount("22334455", 0.00)
+//  val isaDeposited = cashISA.deposit(10000.00)
+//  val withdrawFromISA = isaDeposited.withdraw(200.00)
+//  val personWithCashISA = new Person("Loyal Customer", 56, List(withdrawFromISA))
+//
+//  println(personWithCashISA.speak())
+//
+//  val superAccount = new CashISASavingsAccount(accountNumber = "55667788", balance =  0.00, depositThreshold = 1000.00).deposit(1000.00).withdraw(200.00)
+//  val superUser = new Person("Bryan", 58, List(superAccount))
+//
+//  println(superUser.speak())
 }
